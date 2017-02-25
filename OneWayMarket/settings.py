eae3c36eea89b0f0a39ea75126ca4467104c3df8
Felix Stephen <felsen88@gmail.com>
2017-02-25 23:30:16 +0530

    Modified: Removed - django_extensions

--- OneWayMarket/settings.py
+++ OneWayMarket/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Nifty50',
-    'django_extensions',
+    'Nifty50'
 ]
 
 MIDDLEWARE = [
